a9919f9fdc4d85b4a5fe6ea632434039e01ec4d8
alexru26 <rualex2008@gmail.com>
2024-12-17 11:23:40 -0500

    Master commit
    Signed-off-by: alexru26 <rualex2008@gmail.com>

--- README.md
+++ README.md
@@ -1,6 +1,52 @@
-# Machine Learning Project 04: Poker Bluff Detector
-This is a program that can tell if I'm bluffing or not when playing Poker.
+# Machine Learning Project 04: Poker AI
+This is a program that uses CFR to play Poker (specifically Leduc Hold'em).
+
+## How to run
+Make sure you have the necessary packages.
+
+```
+pip install -r requirements.txt
+```
+
+Run ```main.py``` to use the program.
 
 ## Resources Used
 - ChatGPT
+- https://github.com/datamllab/rlcard
+- https://aipokertutorial.com/
 - https://orb.binghamton.edu/cgi/viewcontent.cgi?article=1028&context=research_days_posters_2021
+- https://github.com/ishikota/PyPokerEngine
+
+## Functionality
+- Model 1: Monte Carlos CFR
+  - Uses policy table to determine what move to take based on state
+  - After traverses down decision tree, goes back up and calculates regret
+  - Regret = How much better this choice would've been
+    - Higher regret = better choice, lower regret = worse choice
+  - Uses regret and action probabilities to calculate gain
+  - Update policy table
+  - Do that for both players
+  - Model trains until reaches Nash Equilibrium
+- Model 2: Deep Monte Carlos CFR 
+  - Uses two deep neural networks to approximate action probabilities and regret
+  - Same procedure as normal MCCFR
+- Model 3: Deep Monte Carlos CFR + Bluff
+  - Same as deep MCCFR but neural networks take into account another integer that represents likelihood opponent is bluffing
+
+## Reasoning and Issues
+- I chose to do Leduc Hold'em instead of No Limit Hold'em because of complexity
+  - Leduc Hold'em has much fewer possibilities
+    - Probably at most 10<sup>5</sup>
+  - No Limit Hold'em has an absurd amount of possibilities
+    - ChatGPT says that there are around 10<sup>160</sup> nodes in the total game tree 
+    - Other teams burned through multiple GPUS to train their CFR model for NLHE
+- Multi-threading during training
+  - Useful since I have to run through the decision for both players independently
+
+## Future Improvements
+- Add bluff classifier
+  - Maybe simulate the rest of game multiple times to see how strong hand is?
+- Collect bluff data throughout game and train model
+  - Essentially building data of player's face
+- Make models faster because can't run normal poker
+  - Pruning?
---
+++ models/deep_cfr_model/Deep_CFR_Model.py
@@ -0,0 +1,231 @@
+import numpy as np
+import collections
+
+import os
+import pickle
+from concurrent.futures import ThreadPoolExecutor
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from rlcard.utils.utils import *
+
+class DeepCFRModel:
+    """
+    Implementation of Deep Monte Carlos CFR algorithm
+    """
+
+    def __init__(self, env, model_path, lr=0.001):
+        """
+        :param env: game environment
+        :param model_path: path to save/load model
+        :param lr: learning rate for neural networks
+        """
+
+        self.use_raw = False
+
+        self.env = env
+        self.model_path = model_path
+        self.iteration = 0
+
+        # Neural networks for policy and regrets
+        input_dim = self.env.state_shape[0][0]  # State feature size
+        num_actions = self.env.num_actions
+
+        self.policy_network = nn.Sequential(
+            nn.Linear(input_dim, 128),
+            nn.ReLU(),
+            nn.Linear(128, num_actions),
+            nn.Softmax(dim=-1)  # Outputs action probabilities
+        )
+        self.regret_network = nn.Sequential(
+            nn.Linear(input_dim, 128),
+            nn.ReLU(),
+            nn.Linear(128, num_actions)  # Outputs regrets for each action
+        )
+
+        # Optimizers
+        self.policy_optimizer = optim.Adam(self.policy_network.parameters(), lr=lr)
+        self.regret_optimizer = optim.Adam(self.regret_network.parameters(), lr=lr)
+
+        # Buffers for training
+        self.policy_memory = []  # [(state, action_probs)]
+        self.regret_memory = []  # [(state, regrets)]
+
+        self.load()
+
+    def train(self):
+        """
+        Do a single iteration of training
+        """
+
+        self.iteration += 1
+        # Use multithreading for parallelizing traversal across players
+        with ThreadPoolExecutor() as executor:
+            futures = [
+                executor.submit(self.parallel_traverse_tree, player_id)
+                for player_id in range(self.env.num_players)
+            ]
+            for future in futures:
+                future.result()  # Ensure all threads complete execution
+
+        # Train the policy and regret networks using collected data
+        self.train_network(self.policy_network, self.policy_optimizer, self.policy_memory, is_policy=True)
+        self.train_network(self.regret_network, self.regret_optimizer, self.regret_memory, is_policy=False)
+
+    def parallel_traverse_tree(self, player_id):
+        """
+        Reset environment and start tree traversal for a specific player.
+        """
+
+        self.env.reset()
+        probs = np.ones(self.env.num_players)
+        self.traverse_tree(probs, player_id)
+
+    def traverse_tree(self, probs, player_id):
+        """
+        Traverse the game tree
+        :param probs: reach probability of current node for players
+        :param player_id: player to update value
+        :return: list of expected utilities for all players
+        """
+
+        if self.env.is_over():
+            return self.env.get_payoffs()
+
+        current_player = self.env.get_player_id()
+        state, legal_actions = self.get_state(current_player)
+
+        action_probs = self.action_probs(state, legal_actions)
+        action_utilities = {}
+        state_utility = np.zeros(self.env.num_players)
+
+        for action in legal_actions:
+            action_prob = action_probs[action]
+            new_probs = probs.copy()
+            new_probs[current_player] *= action_prob
+
+            # Traverse to the child state
+            self.env.step(action)
+            utility = self.traverse_tree(new_probs, player_id)
+            self.env.step_back()
+
+            state_utility += action_prob * utility
+            action_utilities[action] = utility
+
+        if current_player != player_id:
+            return state_utility
+
+        # Compute regret
+        counterfactual_prob = (np.prod(probs[:current_player]) *
+                               np.prod(probs[current_player + 1:]))
+        player_state_utility = state_utility[current_player]
+        regrets = np.zeros(self.env.num_actions)
+        for action in legal_actions:
+            regrets[action] = counterfactual_prob * (action_utilities[action][current_player]
+                                                     - player_state_utility)
+
+        # Store regrets and policy
+        self.regret_memory.append((state, regrets))
+        self.policy_memory.append((state, action_probs))
+
+        return state_utility
+
+    def train_network(self, network, optimizer, memory, is_policy):
+        """
+        Train the neural network
+        :param network: policy or regret network
+        :param optimizer: optimizer for neural network
+        :param memory: stored data
+        :param is_policy: whether the network is policy network
+        """
+
+        batch_size = 32
+        data_size = len(memory)
+        num_batches = max(data_size // batch_size, 1)
+
+        for _ in range(num_batches):
+            batch = [memory[i] for i in np.random.choice(data_size, min(data_size, batch_size), replace=False)]
+
+            states, targets = zip(*batch)
+
+            states_tensor = torch.tensor(np.array(states), dtype=torch.float32)
+            targets_tensor = torch.tensor(np.array(targets), dtype=torch.float32)
+
+            optimizer.zero_grad()
+            outputs = network(states_tensor)
+
+            # Use MSE loss for regrets, and cross-entropy loss for policy
+            if is_policy:
+                loss = nn.CrossEntropyLoss()(outputs, targets_tensor)
+            else:
+                loss = nn.MSELoss()(outputs, targets_tensor)
+
+            loss.backward()
+            optimizer.step()
+
+    def action_probs(self, obs, legal_actions):
+        """
+        Predict action probabilities given state, legal actions
+        :param obs: current state
+        :param legal_actions: list of legal actions
+        :return:
+        """
+
+        state_tensor = torch.tensor(obs, dtype=torch.float32)
+        with torch.no_grad():
+            action_probs = self.policy_network(state_tensor).numpy()
+        action_probs = remove_illegal(action_probs, legal_actions)
+        return action_probs
+
+    def get_state(self, player_id):
+        """
+        Get current state as a string
+        :param player_id: player
+        :return: state as string and indices of legal actions
+        """
+
+        state = self.env.get_state(player_id)
+        return state['obs'], list(state['legal_actions'].keys())
+
+    def eval_step(self, state):
+        """
+        Prediction based on current state
+        :param state: array that represents current state
+        :return: predicted action and dictionary containing info
+        """
+
+        action_probs = self.action_probs(state['obs'], list(state['legal_actions'].keys()))
+        action = np.random.choice(len(action_probs), p=action_probs)
+        return action, {'probs': action_probs}
+
+    def save(self):
+        """
+        Save model
+        """
+
+        if not os.path.exists(self.model_path):
+            os.makedirs(self.model_path)
+
+        torch.save(self.policy_network.state_dict(), f"{self.model_path}/policy_network.pt")
+        torch.save(self.regret_network.state_dict(), f"{self.model_path}/regret_network.pt")
+
+        iteration_file = open(os.path.join(self.model_path, 'iteration.pkl'), 'wb')
+        pickle.dump(self.iteration, iteration_file)
+        iteration_file.close()
+
+    def load(self):
+        """
+        Load model
+        """
+
+        if not os.path.exists(self.model_path):
+            return
+
+        self.policy_network.load_state_dict(torch.load(f"{self.model_path}/policy_network.pt", weights_only=True))
+        self.regret_network.load_state_dict(torch.load(f"{self.model_path}/regret_network.pt", weights_only=True))
+
+        iteration_file = open(os.path.join(self.model_path, 'iteration.pkl'), 'rb')
+        self.iteration = pickle.load(iteration_file)
+        iteration_file.close()
